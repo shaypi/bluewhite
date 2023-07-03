5a9623d158061462c34e04fb823326764a600e3f
Shay <shaypi1986@gmail.com>
2023-07-03 04:26:49 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,12 +3,10 @@ import socket
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def get_hostname():
     hostname = socket.gethostname()
     return f"Hostname: {hostname}"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
