281f52da32ef98adb5db38eaacf47747e954ba5a
Shay <shaypi1986@gmail.com>
2023-07-03 04:27:19 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
+import unittest
 from flask import Flask
 import socket
 
-app = Flask(__name__)
 
-@app.route('/')
-def get_hostname():
-    hostname = socket.gethostname()
-    return f"Hostname: {hostname}"
+class FlaskAppTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+
+    def test_get_hostname(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hostname:', response.data.decode())
+
+    def tearDown(self):
+        pass
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    unittest.main()
